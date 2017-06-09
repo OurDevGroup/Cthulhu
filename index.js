1b11836571ca73c6e2329460c034fd5f22bece95
Ryan Rife <rifer@michaels.com>
2017-06-09 16:33:45 -0500

    clean up

--- index.js
+++ index.js
@@ -4,14 +4,4 @@ var dw = require('./dwdav');
 var git = require('./git');
 var conf = require('./config.json'); //see config.json.sample on how to build this file
 
-var serverOptions = {
-    host: conf.demandware.server,
-    ca: fs.readFileSync(conf.demandware.servercert,'utf8'),
-    cert: fs.readFileSync(conf.demandware.privatekey.certpath,'utf8'),
-    key: fs.readFileSync(conf.demandware.privatekey.keypath,'utf8'),
-    passphrase: conf.demandware.privatekey.password,
-    user: conf.demandware.username,
-    pass: conf.demandware.password  
-}
-
 git.clone();
